363b1d30ff9557e34fd3c331eeb2a303d53a51b0
stunfiskme <brennanhen@gmail.com>
2025-01-20 16:10:09 -0600

    Added Password Hashing

---
+++ Migrations/20250120020748_hasedPasswords.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrennansWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class hasedPasswords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "userAccount",
+                type: "nvarchar(44)",
+                maxLength: 44,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(23)",
+                oldMaxLength: 23);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "salt",
+                table: "userAccount",
+                type: "varbinary(16)",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "salt",
+                table: "userAccount");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "userAccount",
+                type: "nvarchar(23)",
+                maxLength: 23,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(44)",
+                oldMaxLength: 44);
+        }
+    }
+}
